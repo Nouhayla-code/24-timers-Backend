cbc715a1dfb2e915280fbd1a16da93da7f3f94e5
Nouhayla-code <nouhayla@hotmail.dk>
2024-06-18 11:52:44 +0200

    init

---
+++ src/main/java/com/example/timers_24_backend/Timers24BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.timers_24_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Timers24BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Timers24BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/timers_24_backend/Timers24BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.timers_24_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Timers24BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
