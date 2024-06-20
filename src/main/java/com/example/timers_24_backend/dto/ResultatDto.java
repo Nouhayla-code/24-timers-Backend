1c471346cd3292f1ca247511964a112477fe886f
Nouhayla-code <nouhayla@hotmail.dk>
2024-06-20 21:16:16 +0200

    in

--- src/main/java/com/example/timers_24_backend/dto/ResultatDto.java
+++ src/main/java/com/example/timers_24_backend/dto/ResultatDto.java
@@ -1,11 +1,38 @@
 package com.example.timers_24_backend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ResultatDto {
-    public Object getPlacering() {
-        return null;
+
+    private int placering;
+    private String resultat;
+
+
+    // Getter for placering
+    public int getPlacering() {
+        return placering;
     }
 
-    public Object getResultat() {
-        return null;
+    // Setter for placering
+    public void setPlacering(int placering) {
+        this.placering = placering;
+    }
+
+    // Getter for resultat
+    public String getResultat() {
+        return resultat;
+    }
+
+    // Setter for resultat
+    public void setResultat(String resultat) {
+        this.resultat = resultat;
     }
 }
